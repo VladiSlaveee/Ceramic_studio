4a89ad97dfeaaecc9e262de7355874139ce1cb77
Vladislav <vlad.yaloshevski@gmail.com>
2025-06-05 21:18:42 +0300

    slider section

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,44 @@
 import "purecss/build/grids-min.css";
 import "purecss/build/grids-responsive-min.css";
 
+
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import "/src/css/style.css";
+
+
+try {
+    // init Swiper:
+    const swiper = new Swiper('.works__slider', {
+        // configure Swiper to use modules
+        slidesPerView: 1,
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".icon-right-open",
+            prevEl: ".icon-left-open",
+        },
+        breakpoints: {
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 5,
+            },
+            1920: {
+                spaceBetween: 35,
+            }
+        },
+        modules: [Navigation, Pagination],
+    });
+} catch (e) { }
+
+
+
+
